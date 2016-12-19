034593e8a7b36e0a21e4e675df8ead613aaa4d09
Stivenson Rincón <stivenson@miora.co>
2016-12-18 20:40:10 -0500

    function with parameters and ommiting declaration of type

--- functions/functions.go
+++ functions/functions.go
@@ -9,7 +9,13 @@ func add(x int, y int) int {
 	return x + y
 }
 
+func addOmmitType(x, y int) int {
+	return x + y
+}
+
 func main() {
 	fmt.Println(math.Pi)
 	fmt.Println(add(2, 3))
+	fmt.Println(addOmmitType(4, 5))
+
 }
