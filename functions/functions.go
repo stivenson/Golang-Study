02554b9b06add4276bcfc1c011a1f53a96102d3a
Stivenson Rincón <stivenson@miora.co>
2016-12-18 20:43:49 -0500

    multiple results

--- functions/functions.go
+++ functions/functions.go
@@ -13,9 +13,14 @@ func addOmmitType(x, y int) int {
 	return x + y
 }
 
+func multipleResults(x, y string) (string, string) {
+	return x, y
+}
+
 func main() {
 	fmt.Println(math.Pi)
 	fmt.Println(add(2, 3))
 	fmt.Println(addOmmitType(4, 5))
+	fmt.Println(multipleResults("Hello", "Stivenson"))
 
 }
