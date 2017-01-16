134feffda38a516cc25a110e5c32978c8e5e0594
Stivenson Rincón <stivenson@miora.co>
2017-01-16 17:38:14 -0500

    Initialized variables

--- vars/vars.go
+++ vars/vars.go
@@ -5,9 +5,11 @@ import (
 )
 
 var a, b, c int
-
 var x, y, z bool
+var aa, bb, cc int = 1, 2, 3
+var xx, yy, zz = true, "hi", 100
 
 func main() {
 	fmt.Println(a, b, c, x, y, z)
+	fmt.Println(aa, bb, cc, xx, yy, zz)
 }
