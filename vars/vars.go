b38b39f747cec6e2cd3207ded5b5da77e12f20b8
Stivenson Rincón <stivenson@miora.co>
2017-01-16 17:44:20 -0500

    Implicit declaration of variables

--- vars/vars.go
+++ vars/vars.go
@@ -10,6 +10,11 @@ var aa, bb, cc int = 1, 2, 3
 var xx, yy, zz = true, "hi", 100
 
 func main() {
+	var o, n string = "Aja", "y tu que"
+	h, l, m := true, false, "Hello"
+
 	fmt.Println(a, b, c, x, y, z)
 	fmt.Println(aa, bb, cc, xx, yy, zz)
+	fmt.Println(h, l, m)
+	fmt.Println(o, n)
 }
