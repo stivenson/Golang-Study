c05f3dc1b45aed14d4e8e140939d025ef4714dfd
Stivenson Rincón <stivenson@miora.co>
2017-01-17 15:55:56 -0500

    Numeric Constans

--- consts/consts.go
+++ consts/consts.go
@@ -6,9 +6,21 @@ import (
 
 const Pi = 3.1416
 
+// Numeric constans
+
+const (
+	Big   = 1 << 100  // Bits duplicate to 100
+	Small = Big >> 99 // Bits reduce to 99
+)
+
+func needFloat(f float64) float64 {
+	return f * 0.1
+}
+
 func main() {
 	const cn1 = "a text"
 	const cn2 = 2
 	const cn3 = true
 	fmt.Println(cn1, cn2, cn3, Pi)
+	fmt.Println(needFloat(Big), Small)
 }
