4961a95d4c6c3c4f0b91789900d95f622071ecb4
Stivenson Rincón <stivenson@miora.co>
2017-01-17 18:01:24 -0500

    Exercise: refact square root with Newton method

--- exercises/cyclesfunctions.go
+++ exercises/cyclesfunctions.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 )
 
+// Function for square root with newton method
 func Sqrt(x float64) float64 {
-	z := float64(0)
-	n := float64(1)
+	z, n := float64(0), float64(1)
 	for float32(z) <= float32(x) {
 		n += 0.001
 		z = float64(n * n)
